d2d79b49a3bf6e4db7f54a182157c042422bf94a
MohnadBabgi <mohanadbabgi@gmail.com>
2025-09-17 22:52:52 +0300

    Setting header and adding status code in server.js

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const PORT = 8000
 const server = http.createServer(async (req, res) => {
   const destinations = await getDataFromDB()
 
-
+    res.setHeader('Content-Type', 'application/json')
+    res.statusCode = 200;
   if (req.url === '/api' && req.method === 'GET') {
     res.end(JSON.stringify(destinations))
   }
